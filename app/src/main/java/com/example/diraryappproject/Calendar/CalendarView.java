d36ec9bdb1a9e89c528bf4db7fc8b6bfe9decbbd
soohyeon13 <rlatngus6663@gmail.com>
2019-05-31 15:37:50 +0900

    Calendar

--- app/src/main/java/com/example/diraryappproject/Calendar/CalendarView.java
+++ app/src/main/java/com/example/diraryappproject/Calendar/CalendarView.java
@@ -1,9 +1,11 @@
 package com.example.diraryappproject.Calendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.diraryappproject.R;
+import com.tyczj.extendedcalendarview.ExtendedCalendarView;
 
 public class CalendarView extends AppCompatActivity {
     @Override
@@ -11,6 +13,10 @@ public class CalendarView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendarview);
 
+        Intent intent = new Intent(this.getIntent());
+
+
+        ExtendedCalendarView extendedCalendarView = findViewById(R.id.calendar);
 
 
     }
--- app/src/main/java/com/example/diraryappproject/MainActivity.java
+++ app/src/main/java/com/example/diraryappproject/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.diraryappproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-import com.tyczj.extendedcalendarview.ExtendedCalendarView;
+import com.example.diraryappproject.Calendar.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExtendedCalendarView calendar = (ExtendedCalendarView)findViewById(R.id.calendar);
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalendarView.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
