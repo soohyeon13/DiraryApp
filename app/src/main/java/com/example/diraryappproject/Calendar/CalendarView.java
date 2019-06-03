9dbf82e74724ae8213a75b520b28141407194e32
soohyeon13 <rlatngus6663@gmail.com>
2019-06-04 05:05:46 +0900

    수정작업

--- app/src/main/java/com/example/diraryappproject/Calendar/CalendarView.java
+++ app/src/main/java/com/example/diraryappproject/Calendar/CalendarView.java
@@ -1,118 +1,15 @@
 package com.example.diraryappproject.Calendar;
 
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Toast;
 
 import com.example.diraryappproject.R;
-import com.tyczj.extendedcalendarview.Day;
-import com.tyczj.extendedcalendarview.ExtendedCalendarView;
-import com.tyczj.extendedcalendarview.UserAdapter;
-import com.tyczj.extendedcalendarview.UserCollection;
 
-import java.util.ArrayList;
-import java.util.GregorianCalendar;
-
-public class CalendarView extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    ActionBarDrawerToggle drawerToggle;
-    Toolbar toolbar;
-    private GregorianCalendar calMonth,calMonthCopy;
-    private com.tyczj.extendedcalendarview.UserAdapter UserAdapter;
-
+public class CalendarView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calendarview);
-        UserCollection.userCollectionList = new ArrayList<UserCollection>();
-        UserCollection.userCollectionList.add(new UserCollection("2019-06-04","rlatngus","test"));
+        setContentView(R.layout.drawer);
 
-        calMonth = (GregorianCalendar) GregorianCalendar.getInstance();
-        calMonthCopy = (GregorianCalendar) calMonth.clone();
-
-        UserAdapter = new UserAdapter(CalendarView.this,calMonth,UserCollection.userCollectionList);
-
-
-        Intent intent = new Intent(this.getIntent());
-
-        ExtendedCalendarView extendedCalendarView = findViewById(R.id.calendar);
-
-        extendedCalendarView.setOnDayClickListener(new ExtendedCalendarView.OnDayClickListener() {
-            @Override
-            public void onDayClicked(AdapterView<?> adapter, View view, int position, long id, Day day) {
-
-            }
-        });
-        initLayout();
-    }
-
-    private void initLayout() {
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(" ");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        drawerLayout= findViewById(R.id.dl_main_drawer_root);
-        navigationView = findViewById(R.id.navView);
-        drawerToggle = new ActionBarDrawerToggle(
-                this, drawerLayout,toolbar,
-                R.string.drawer_open, R.string.drawer_close
-        );
-        drawerLayout.addDrawerListener(drawerToggle);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.item1:
-                Toast.makeText(this,"item1 select",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.item2:
-                Toast.makeText(this,"item2 select",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.item3:
-                Toast.makeText(this,"item3 select",Toast.LENGTH_SHORT).show();
-                break;
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return false;
-    }
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }else {
-            super.onBackPressed();
-        }
     }
 }
---
+++ app/src/main/java/com/example/diraryappproject/Calendar/UserCollection.java
@@ -0,0 +1,16 @@
+package com.example.diraryappproject.Calendar;
+
+import java.util.ArrayList;
+
+public class UserCollection {
+    public static ArrayList<UserCollection> userCollectionList;
+    public String description="";
+    public String name="";
+    public String date ="";
+
+    public UserCollection(String date,String name,String description) {
+        this.date = date;
+        this.name =name;
+        this.description = description;
+    }
+}
